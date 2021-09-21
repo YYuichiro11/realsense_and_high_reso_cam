c4d65b9b0151d82acf389cf32c35013b76348db8
YYuichiro11 <51150379+YYuichiro11@users.noreply.github.com>
2021-09-21 18:08:10 +0900

    20210921

--- codes/sony_test.py
+++ codes/sony_test.py
@@ -6,77 +6,55 @@ Created on Thu Sep 16 16:16:44 2021
 @author: yuichiro
 
 ここでは，sonyのα7cをpython上で制御し，1秒に一枚程度の間隔で撮影を行います
+下記に従ってやってみる
+https://github.com/Bloodevil/sony_camera_api
+
+これらは、動画からフレームを切り取るだけ
 https://www.yuseiphotos.work/entry/sonywebcam
 https://yusei-roadstar.hatenablog.com/entry/2019/11/19/193312
 
 """
+import sys
+import cv2
+import time
+import datetime
 
 
-# import cv2
-
-# ##############  Camera capture ################################
-# cap = cv2.VideoCapture(1)
-# cols = 1920
-# rows = 1080
-# framerate = 60
-
-
-
-# cap.set(cv2.CAP_PROP_FPS, framerate)      #カメラFPSを60FPSに設定
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, cols)   #カメラ画像の横幅を1920設定
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, rows)  #カメラ画像の縦幅を1080に設定
-
-
-# print (cap.get(cv2.CAP_PROP_FPS))
-# print (cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# print (cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-
-# while(True):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-
-
-#     cv2.namedWindow("frame", cv2.WINDOW_KEEPRATIO | cv2.WINDOW_NORMAL)
-#     cv2.imshow('frame',frame)
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-
-#     cv2.imshow("result", frame)
-
-
-
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
-
+# カメラが使えるかどうかテストします
 
 # https://qiita.com/opto-line/items/7ade854c26a50a485159
 
-import cv2
-
 # VideoCapture オブジェクトを取得します
-capture = cv2.VideoCapture(2)
+cc = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = capture.read()
-    # resize the window
-    windowsize = (800, 600)
-    frame = cv2.resize(frame, windowsize)
+while True:
+    # ret, frame = cc.read()
+    # # resize the window
+    # 
 
-    cv2.imshow('title',frame)
+    # 
+    
+    rr, img = cc.read()
+    
+    windowsize = (3000,2000)
+    img = cv2.resize(img, windowsize)
+    # cv2.imshow('title',img)
+    
+    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    cv2.imwrite( now + '.jpg',img)
+    time.sleep(0.8)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-capture.release()
+cc.release()
 cv2.destroyAllWindows()
 
 
-# import cv2
-# import datetime
 
+
+
+
+# カメラの情報を読み込みます
 
 # def check_camera_connection():
 #     """
